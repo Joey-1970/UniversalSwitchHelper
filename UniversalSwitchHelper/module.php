398236a030effa80046a351e67a717d936ac7482
Joey-1970 <paeper@horburg.de>
2024-12-13 15:09:30 +0100

    Create module.php

---
+++ UniversalSwitchHelper/module.php
@@ -0,0 +1,82 @@
+<?
+    // Klassendefinition
+    class UniversalSwitchHelper extends IPSModule 
+    { 
+	// Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() 
+        {
+            	// Diese Zeile nicht löschen.
+            	parent::Create();
+ 	    	$this->RegisterPropertyBoolean("Open", false);
+	
+        }
+ 	
+	public function GetConfigurationForm() 
+	{ 
+		$arrayStatus = array(); 
+		$arrayStatus[] = array("code" => 101, "icon" => "inactive", "caption" => "Instanz wird erstellt"); 
+		$arrayStatus[] = array("code" => 102, "icon" => "active", "caption" => "Instanz ist aktiv");
+		$arrayStatus[] = array("code" => 104, "icon" => "inactive", "caption" => "Instanz ist inaktiv");
+				
+		$arrayElements = array(); 
+		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
+ 		
+		$arrayActions = array();
+		$arrayActions[] = array("type" => "Label", "label" => "Test Center"); 
+		$arrayActions[] = array("type" => "TestCenter", "name" => "TestCenter");
+		
+ 		return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements, "actions" => $arrayActions)); 		 
+ 	}       
+	   
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() 
+        {
+            	// Diese Zeile nicht löschen
+            	parent::ApplyChanges();
+		
+		// Profil anlegen
+		
+	
+		// Statusvariablen
+		
+		
+		If ($this->HasActiveParent() == true) {	
+			If ($this->ReadPropertyBoolean("Open") == true) {
+				If ($this->GetStatus() <> 102) {
+					$this->SetStatus(102);
+				}
+			}
+			else {
+				If ($this->GetStatus() <> 104) {
+					$this->SetStatus(104);
+				}
+			}
+		}
+	}
+	
+	public function RequestAction($Ident, $Value) 
+	{
+		switch($Ident) {
+		case "VPN_Connect":
+			$this->VPN_Connect();
+			break;
+		
+	
+		default:
+		    throw new Exception("Invalid Ident");
+		}
+	}
+	    
+	// Beginn der Funktionen
+
+	    
+	
+	 
+	
+
+
+	
+	    
+
+}
+?>
